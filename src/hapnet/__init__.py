f9d9021a7bfd18571bcd7ef6692f99677f806d8a
Andrew Davinack <davinack_drew@wheatoncollege.edu>
2026-06-03 22:10:48 -0400

    Update __init__.py

--- src/hapnet/__init__.py
+++ src/hapnet/__init__.py
@@ -1,13 +1,16 @@
+__version__ = "0.2.1"
+
 __all__ = [
     "read_fasta",
     "build_haplotypes",
     "build_mst_network",
     "plot_network",
     "write_logs",
+    "write_run_metadata",
 ]
 
 from .io import read_fasta
 from .haplotypes import build_haplotypes
 from .network import build_mst_network
 from .plot import plot_network
-from .logs import write_logs
+from .logs import write_logs, write_run_metadata
